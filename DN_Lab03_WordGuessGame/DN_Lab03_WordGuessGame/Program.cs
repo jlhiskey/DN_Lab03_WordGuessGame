e06c7578a0441dd26dbbc5a5b913b99e54aa7cc3
Jason Hiskey <jasonhi@crazyredhead.com>
2019-01-09 20:47:17 -0800

    Sketched Out Basic Application Logic and Test Logic

---
+++ DN_Lab03_WordGuessGame/DN_Lab03_WordGuessGame/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace DN_Lab03_WordGuessGame
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //Home Navigation
+
+            //Modify Word List
+
+                //View Words Link
+
+                //Add Word Link
+
+                //Remove Word Link
+
+            //Start New Game
+
+            //Exit Application
+
+        //View Words in External File
+
+            //ReadFile
+
+            //Split Words into Array
+
+            //Print Words
+
+        //Add Word to External File
+
+            //AppendToFile
+
+        //Remove Word from External File
+
+            //ReadFile
+
+            //Split Words into Array
+
+            //Find Word and remove from Array
+
+            //CreateFile
+
+        //Exit Game
+
+            //Home Navigation Link
+
+        //Start New Game
+
+            //Select Random Word
+
+            //Ask User to Input Char
+
+            //Respond With Correct/Incorrect
+
+            //Keep Track of Char Used
+
+            //Tell Them They Won
+
+    }
+}
---
+++ DN_Lab03_WordGuessGame/WordGuessGameTestSuite/UnitTest1.cs
@@ -0,0 +1,38 @@
+using System;
+using Xunit;
+using DN_Lab03_WordGuessGame;
+
+namespace WordGuessGameTestSuite
+{
+    public class FileAccess
+    {
+        [Fact]
+        public void TestingThatAFileCanBeUpdated()
+        {
+
+        }
+        [Fact]
+        public void TestingThatAWordCanBeAdded()
+        {
+
+        }
+        [Fact]
+        public void TestingThatYouCanRetreiveAllWordsFromFile()
+        {
+
+        }
+    }
+    public class GameLogic
+    {
+        [Fact]
+        public void TestingThatLetterIsFoundInWord()
+        {
+
+        }
+        [Fact]
+        public void TestingThatLetterIsNotFoundInWord()
+        {
+
+        }
+    }
+}
